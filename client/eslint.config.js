0926f3da6edc3b071d29fdee432dc51280b2457e
ilkol <ilkolreserv@gmail.com>
2026-04-10 19:34:44 +0400

    add tailwind

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -23,7 +23,10 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['warn', { 
+		varsIgnorePattern: '^[A-Z]', // Игнорировать переменные, начинающиеся с большой буквы (компоненты)
+		argsIgnorePattern: '^_' 
+	  }],
     },
   },
 ])
