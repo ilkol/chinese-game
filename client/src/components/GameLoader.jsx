16ad4aa43fd0c40b4e9ad44d5ad779b00cb590ad
ilkol <ilkolreserv@gmail.com>
2026-05-20 19:36:32 +0400

    Added a delay in the loader

--- client/src/components/GameLoader.jsx
+++ client/src/components/GameLoader.jsx
@@ -1,32 +1,60 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
-const GameLoader = ({lightMod} = {lightMod: false}) => (
-	<div className={`h-screen bg-[#${lightMod ? "ffffff" : "020617"}] flex flex-col items-center justify-center`}>
-		<div className="relative">
-			{/* Внешнее кольцо */}
-			<motion.div
-				animate={{ rotate: 360 }}
-				transition={{ repeat: Infinity, duration: 4, ease: "linear" }}
-				className="w-32 h-32 border-4 border-dashed border-blue-500/30 rounded-full"
-			/>
-			{/* Внутренний иероглиф */}
-			<motion.div
-				animate={{ scale: [1, 1.2, 1], opacity: [0.5, 1, 0.5] }}
-				transition={{ repeat: Infinity, duration: 2 }}
-				className="absolute inset-0 flex items-center justify-center text-4xl text-blue-400 font-bold"
-			>
-				龙
-			</motion.div>
-		</div>
-		<motion.p
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			transition={{ delay: 0.5 }}
-			className="mt-8 text-blue-200/50 uppercase tracking-[0.3em] text-xs font-black"
-		>
-			Подготовка экспедиции...
-		</motion.p>
-	</div>
-);
+const GameLoader = ({ lightMod = false, isLoading = true }) => {
+	const [delayPassed, setDelayPassed] = useState(false);
+
+	useEffect(() => {
+		let timeout;
+
+		if (isLoading) {
+			// Включаем лоадер через 600мс
+			timeout = setTimeout(() => {
+				setDelayPassed(true);
+			}, 600);
+		} else {
+			timeout = setTimeout(() => {
+				setDelayPassed(false);
+			}, 0);
+		}
+
+		return () => clearTimeout(timeout);
+	}, [isLoading]);
+
+	const isVisible = isLoading && delayPassed;
+
+	return (
+		<AnimatePresence>
+			{isVisible && (
+				<div className={`h-screen bg-[#${lightMod ? "ffffff" : "020617"}] flex flex-col items-center justify-center`}>
+					<div className="relative">
+						{/* Внешнее кольцо */}
+						<motion.div
+							animate={{ rotate: 360 }}
+							transition={{ repeat: Infinity, duration: 4, ease: "linear" }}
+							className="w-32 h-32 border-4 border-dashed border-blue-500/30 rounded-full"
+						/>
+						{/* Внутренний иероглиф */}
+						<motion.div
+							animate={{ scale: [1, 1.2, 1], opacity: [0.5, 1, 0.5] }}
+							transition={{ repeat: Infinity, duration: 2 }}
+							className="absolute inset-0 flex items-center justify-center text-4xl text-blue-400 font-bold"
+						>
+							龙
+						</motion.div>
+					</div>
+					<motion.p
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ delay: 0.5 }}
+						className="mt-8 text-blue-200/50 uppercase tracking-[0.3em] text-xs font-black"
+					>
+						Подготовка экспедиции...
+					</motion.p>
+				</div>
+			)}
+		</AnimatePresence>
+	)
+};
 
 export default GameLoader;
